f74ad092b3ca6777a85c480810ab880806e4e872
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-07-09 10:36:02 +0200

    prove con methods components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ title }}</h1>
+    <input type="text" ref="name">
+    <button @click="handleClick">Click</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  name: "App",
+  data() {
+    return {
+      title: "My First Vue Cli app",
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs);
+      this.$refs.name.classList.add('active');
+      this.$refs.name.focus();
+    },
   },
 };
 </script>
